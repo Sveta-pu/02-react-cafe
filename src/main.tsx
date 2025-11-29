06e0194353b19855a713dfee1dfaf2c20a1436bb
Sveta <pupsadon@gmail.com>
2025-11-29 22:28:47 +0200

    Implement vote logic: add totalVotes, positiveRate, conditional rendering and Notification component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
+import 'modern-normalize';
 
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// // import App from './components/App/App';
-// // Нормалізація стилів
-// import 'modern-normalize';
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>{/* <App /> */}</React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
